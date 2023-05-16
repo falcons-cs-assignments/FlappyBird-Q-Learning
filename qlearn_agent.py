d01d40e5db61f019f1ef5eff2b72b3bd98a5a25b
Mohamed-Adel <mohamedadel662001200@gmail.com>
2023-05-16 23:24:15 +0300

    Q_table has built as nparray --state_index_map() isn't implemented

--- qlearn_agent.py
+++ qlearn_agent.py
@@ -7,12 +7,12 @@ class Q_learn:
         self.init_state_index = self.state_index_map(state)
         self.state_index = self.init_state_index
         self.next_state_index = None
-        self.action = "jump"
+        self.action_index = 1
         # Initialize Q-table
-        self.num_states = (58,  # buckets num of bird_y
-                           2,   # buckets num of bird_velocity
-                           16,  # buckets num of gap_x
-                           24   # buckets num of gap_y
+        self.num_states = (58,  # num of bird_y buckets
+                           2,   # num of bird_velocity buckets
+                           16,  # num of gap_x buckets
+                           24   # num of gap_y buckets
                            )
         self.num_actions = (2,)  # It's a tuple
         self.Q = np.zeros(self.num_states + self.num_actions)
@@ -27,7 +27,7 @@ class Q_learn:
 
     def reset(self):
         self.state_index = self.init_state_index
-        self.action = "jump"
+        self.action_index = 1
 
     def state_index_map(self, state):
         # Todo: convert continuous state to a discrete index
@@ -53,39 +53,44 @@ class Q_learn:
         }
         """
 
-        state_index = {
-
-        }
-        return index
-        pass
+        state_index = ()  # It's a tuple to be used in indexing a np array
+        return state_index
 
     # Define a function to select an action using epsilon-greedy strategy
-    def epsilon_greedy(self, state):
+    def epsilon_greedy(self, state_index):
         # Choose a random action with probability epsilon
         if random.uniform(0, 1) < self.epsilon:
-            action = random.choice(list(self.Q[state].keys()))
+            action_index = random.randrange(0, self.Q[state_index].size)
         # Otherwise, choose the action with the highest Q-value
         else:
-            max_value = max(self.Q[state].values())
-            actions = [a for a, v in self.Q[state].items() if v == max_value]
-            action = random.choice(actions)
-        return action
+            max_value = max(self.Q[state_index])
+            actions_indices = [i for i, v in self.Q[state_index] if v == max_value]
+            action_index = random.choice(actions_indices)
+
+        self.action_index = action_index
+        return action_index
 
     def take_action(self, state, exploration=True):
+        """
+        It maps "action_index = 1" to "jump"
+        and "action_index = 0" to " " meaning no jump
+        """
+        state_index = self.state_index_map(state)
         if exploration:  # True during learning
-            self.action = self.epsilon_greedy(state)
+            action = "jump" if self.epsilon_greedy(state_index) else " "
         else:
-            max_value = max(self.Q[state].values())
-            actions = [a for a, v in self.Q[state].items() if v == max_value]
-            self.action = random.choice(actions)
-        return self.action
+            max_value = max(self.Q[state_index])
+            actions_indices = [i for i, v in self.Q[state_index] if v == max_value]
+            action_index = random.choice(actions_indices)
+            action = "jump" if action_index else " "
+        return action
 
     # Q-learning algorithm
     def learn(self, state, reward, done=False):
         self.next_state_index = self.state_index_map(state)
         # Update Q-value for state-action pair
-        td_error = reward + self.gamma * np.max(self.Q[self.next_state_index]) - self.Q[self.state_index][self.action]
-        self.Q[self.state_index][self.action] += self.alpha * td_error
+        td_error = reward + self.gamma * np.max(self.Q[self.next_state_index]) - self.Q[self.state_index + (self.action_index,)]
+        self.Q[self.state_index + (self.action_index,)] += self.alpha * td_error
 
         # update state
         self.state_index = self.next_state_index
